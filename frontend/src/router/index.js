854575b201928c14aaa82dc79ab4334d994026f0
HUISOO BYUN <gmltn0907@naver.com>
2024-11-24 19:00:08 +0900

    예적금, 환율, 지도 추가 수정 필요

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -5,12 +5,16 @@ import SignUpView from '@/views/SignUpView.vue'
 import LogInView from '@/views/LogInView.vue'
 import MyPageView from '@/views/MyPageView.vue'
 import Compare from '@/components/Compare.vue'
+import CompareListView from '@/views/CompareListView.vue'
+import DepositList from '@/components/DepositList.vue'
+import SavingList from '@/components/SavingList.vue'
+import MapView from '@/views/MapView.vue'
 import { useCounterStore } from '@/stores/counter'
 import HomeView from '../views/HomeView.vue'
 import MainPage from '../components/MainPage.vue'
 import CurrencyConverter from '../views/CurrencyConverter.vue'
 import ExchangeRates from '../views/ExchangeRates.vue'
-
+import NotFound from '@/components/NotFound.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -62,8 +66,33 @@ const router = createRouter({
     },
     {
       path: '/compare',
-      name: 'Compare',
-      component: Compare,
+      component: CompareListView,
+      children: [
+        {
+          path: 'deposit',
+          name: 'depositList',
+          component: DepositList,
+        },
+        {
+          path: 'saving',
+          name: 'savingList',
+          component: SavingList,
+        },
+      ]
+    },
+    {
+      path: '/bank',
+      name: 'bankMap',
+      component: MapView
+    },
+    {
+      path: '/404',
+      name: 'notFound',
+      component: NotFound
+    },
+    {
+      path: '/:pathMatch(.*)*',
+      redirect: "/404"
     },
   ]
 })
@@ -71,3 +100,141 @@ const router = createRouter({
 
 
 export default router
+
+// import { createRouter, createWebHistory } from 'vue-router';
+
+// import HomeView from '@/views/HomeView.vue';
+// import NotFound from '@/components/NotFound.vue';
+// import CompareListView from '@/views/CompareListView.vue';
+// import DepositList from '@/components/DepositList.vue';
+// import SavingList from '@/components/SavingList.vue';
+// import ExchangeView from '@/views/ExchangeView.vue';
+// import MapView from '@/views/MapView.vue';
+// import PostListView from '@/views/PostListView.vue';
+// import PostDetailView from '@/views/PostDetailView.vue';
+// import PostCreateView from '@/views/PostCreateView.vue';
+// import PostUpdateView from '@/views/PostUpdateView.vue';
+// import SignUpView from '@/views/SignUpView.vue';
+// import SignInView from '@/views/SignInView.vue';
+// import MyPageView from '@/views/MyPageView.vue';
+// import MyPage from '@/components/MyPage.vue';
+// import ProductManage from '@/components/ProductManage.vue';
+// import ProductRecommend from '@/components/ProductRecommend.vue';
+// import CreateView from '@/views/CreateView.vue';
+// import CurrencyConverter from '@/views/CurrencyConverter.vue';
+// import ExchangeRates from '@/views/ExchangeRates.vue';
+// import Compare from '@/components/Compare.vue';
+
+// const router = createRouter({
+//   history: createWebHistory(import.meta.env.BASE_URL),
+//   routes: [
+//     {
+//       path: '/',
+//       name: 'home',
+//       component: HomeView,
+//     },
+//     {
+//       path: '/create',
+//       name: 'CreateView',
+//       component: CreateView,
+//     },
+//     {
+//       path: '/signup',
+//       name: 'SignUpView',
+//       component: SignUpView,
+//     },
+//     {
+//       path: '/signin',
+//       name: 'SignInView',
+//       component: SignInView,
+//     },
+//     {
+//       path: '/mypage/:username*',
+//       component: MyPageView,
+//       children: [
+//         {
+//           path: '',
+//           name: 'myPage',
+//           component: MyPage,
+//         },
+//         {
+//           path: 'products',
+//           name: 'productManage',
+//           component: ProductManage,
+//         },
+//         {
+//           path: 'recommend',
+//           name: 'productRecommend',
+//           component: ProductRecommend,
+//         },
+//       ],
+//     },
+//     {
+//       path: '/compare',
+//       component: CompareListView,
+//       children: [
+//         {
+//           path: 'deposit',
+//           name: 'depositList',
+//           component: DepositList,
+//         },
+//         {
+//           path: 'saving',
+//           name: 'savingList',
+//           component: SavingList,
+//         },
+//       ],
+//     },
+//     {
+//       path: '/compare-simple',
+//       name: 'Compare',
+//       component: Compare,
+//     },
+//     {
+//       path: '/bank',
+//       name: 'bankMap',
+//       component: MapView,
+//     },
+//     {
+//       path: '/posts',
+//       name: 'postList',
+//       component: PostListView,
+//     },
+//     {
+//       path: '/posts/:id',
+//       name: 'postDetail',
+//       component: PostDetailView,
+//     },
+//     {
+//       path: '/posts/write',
+//       name: 'postCreate',
+//       component: PostCreateView,
+//     },
+//     {
+//       path: '/posts/:id/update',
+//       name: 'postUpdate',
+//       component: PostUpdateView,
+//     },
+//     {
+//       path: '/CurrencyConverter',
+//       name: 'CurrencyConverter',
+//       component: CurrencyConverter,
+//     },
+//     {
+//       path: '/exchange-rates',
+//       name: 'ExchangeRates',
+//       component: ExchangeRates,
+//     },
+//     {
+//       path: '/404',
+//       name: 'notFound',
+//       component: NotFound,
+//     },
+//     {
+//       path: '/:pathMatch(.*)*',
+//       redirect: '/404',
+//     },
+//   ],
+// });
+
+// export default router;
